aa3c487eb188c14bf8b519559e65d90ec784927c
subham007-coder <dsubham109@gmail.com>
2024-01-15 13:14:46 +0530

    add

--- demo/app.js
+++ demo/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const ejs = require('ejs');
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("./public"))
 
 app.get('/', function (req, res) {
-  res.render('index')
+  res.render('index');
 })
 
 app.listen(3000);
